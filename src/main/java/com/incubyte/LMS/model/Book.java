174386d472a19147681286fbaad02178be101928
Sandeep <sandeep.mudigonda@zemosolabs.com>
2024-12-26 10:40:42 +0530

    changes made in book entiry and application.properties

--- src/main/java/com/incubyte/LMS/model/Book.java
+++ src/main/java/com/incubyte/LMS/model/Book.java
@@ -4,24 +4,66 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@AllArgsConstructor
-@Getter
-@Setter
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String isbn;
     private String title;
     private String author;
-    private int year;
-    private boolean available = true;
+    private String isbn;
+    private boolean available;
+    private int publishYear;
 
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
 }
-
